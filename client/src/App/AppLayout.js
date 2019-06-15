bc2ac824e6c7d8e608eb82a4c03327fd15fe2a23
codejoncode <jonathanjholloway@gmail.com>
2019-06-15 11:19:42 -0500

    created a styled component to use as a wrapper added in to surround all elements in the app component

---
+++ client/src/App/AppLayout.js
@@ -0,0 +1,6 @@
+import styled from 'styled-components';
+
+/*Using as a wrapper */
+export default styled.div`
+    padding: 40px;
+`;
--- client/src/App/index.js
+++ client/src/App/index.js
@@ -1,16 +1,16 @@
 import React, {Component} from 'react';
 import './App.css';
 import WelcomeMessage from './WelcomeMessage';
-// import styled, { css } from 'styled-components';
+import AppLayout from '../App/AppLayout';
 
 
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <AppLayout>
         <WelcomeMessage name = "Cyrpto"/>
-      </div>
+      </AppLayout>
     );
   }
 
