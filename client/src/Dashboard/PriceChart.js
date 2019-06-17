dd14151b1926da3b13cb85450634e3d0f0bb5f67
codejoncode <jonathanjholloway@gmail.com>
2019-06-17 15:40:44 -0500

    implemented the high charts and adjusted the styling to match the current template

--- client/src/Dashboard/PriceChart.js
+++ client/src/Dashboard/PriceChart.js
@@ -3,15 +3,17 @@ import highChartsConfig from "./highChartsConfig";
 import { Title } from "../Shared/Title";
 import { AppContext } from "../App/AppProvider";
 import ReactHighcharts from "react-highcharts";
+import ChartsTheme from "./ChartsTheme";
 
+ReactHighcharts.Highcharts.setOptions(ChartsTheme);
 export default () => {
   return (
     <AppContext.Consumer>
-      {({}) => 
-      <Title>
-        <ReactHighcharts config={highChartsConfig()} />
-      </Title>
-    }
+      {({}) => (
+        <Title>
+          <ReactHighcharts config={highChartsConfig()} />
+        </Title>
+      )}
     </AppContext.Consumer>
   );
 };
