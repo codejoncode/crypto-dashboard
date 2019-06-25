9bfb8b2f8f93344bf5969a8784e1c18be2bb7ce5
codejoncode <jonathanjholloway@gmail.com>
2019-06-24 23:23:07 -0500

    added a segment for the landing page. Still considering ideas for it but I think this will look better than the current page

--- client/src/App/Home/LandingPage.js
+++ client/src/App/Home/LandingPage.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-
+import { Button, Header, Icon, Segment } from 'semantic-ui-react'
 class LandingPage extends Component {
   authenticateUser = async () => {
     console.log(`Am I authenticated `);
@@ -8,11 +8,26 @@ class LandingPage extends Component {
   render() {
     return (
       <div>
-        <p>Login Required</p>
-        <button onClick={this.props.loginUser}>
+        {/* <p>Login Required</p>
+        <button onClick={this.props.loginUser}> */}
           {/* <a href="https://crypto-dashboard.auth0.com/login?client=ZEvOn2qtAc5TyZmhttSQaqara1AzA4Ez">Login</a> */}
-          Login
-        </button>
+          {/* Login
+        </button> */}
+        <div>
+        {/* <Segment circular style={square}>
+          <Header as='h2'>
+            Sale!
+            <Header.Subheader>$10.99</Header.Subheader>
+          </Header>
+        </Segment> */}
+        <Segment circular inverted style={square} textAlign = 'center' onClick={this.props.loginUser}>
+          <Header as='h2' inverted>
+            Sign Up
+            <Header.Subheader>or Login In</Header.Subheader>
+          </Header>
+        </Segment>
+      </div>
+
       </div>
     );
   }
